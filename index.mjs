ae835615492b74e864fa5c87ddc961c2b9250976
Soma Lucz <luczsoma@gmail.com>
2020-07-25 22:53:30 +0200

    Set 1 / Challenge 2

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 import { challenge_1 } from "./set_1/challenge_1";
+import { challenge_2 } from "./set_1/challenge_2";
 
 function main() {
   console.log(`Set 1 / Challenge 1: ${challenge_1() ? "OK" : "error"}`);
+  console.log(`Set 1 / Challenge 2: ${challenge_2() ? "OK" : "error"}`);
 }
 
 main();
---
+++ set_1/challenge_2.mjs
@@ -0,0 +1,25 @@
+export function equalLengthHexXor(hex1, hex2) {
+  const evenPaddedHex1 = hex1.length % 2 === 0 ? hex1 : `0${hex1}`;
+  const evenPaddedHex2 = hex2.length % 2 === 0 ? hex2 : `0${hex2}`;
+
+  if (evenPaddedHex1.length !== evenPaddedHex2.length) {
+    throw new Error("inputs are not of same size");
+  }
+
+  const buffer1 = Buffer.from(evenPaddedHex1, "hex");
+  const buffer2 = Buffer.from(evenPaddedHex2, "hex");
+
+  const result = Buffer.alloc(buffer1.length);
+  for (let i = 0; i < buffer1.length; i++) {
+    result[i] = buffer1[i] ^ buffer2[i];
+  }
+  return result.toString("hex");
+}
+
+export function challenge_2() {
+  const hex1 = "1c0111001f010100061a024b53535009181c";
+  const hex2 = "686974207468652062756c6c277320657965";
+  const expectedHexXorResult = "746865206b696420646f6e277420706c6179";
+  const actualHexXorResult = equalLengthHexXor(hex1, hex2);
+  return expectedHexXorResult === actualHexXorResult;
+}
